7ec5562946d4c1bf68c90e7bfa9c7d28859d9d7d
Michele De Guzman Murata <106496883+mmurata22@users.noreply.github.com>
2024-04-14 15:17:38 -0400

    Finished Navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var hideButton = document.getElementById("hideButton");
-    var leftBox = document.querySelector(".left-box");
-  
-    hideButton.addEventListener("click", function() {
-      leftBox.style.display = "none";
-    });
+  var myDiv = document.getElementById("nav");
+  var closeButton = document.getElementById("closeIcon");
+  var openButton = document.getElementById("navIcon");
+
+  closeButton.addEventListener("click", function() {
+    myDiv.style.display = "none";
   });
+
+  navIcon.addEventListener("click", function() {
+    myDiv.style.display = "block";
+  });
+});
+
